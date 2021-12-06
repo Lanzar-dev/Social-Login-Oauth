a19790c8f7ba2f67efdf172bf56e45adb52fe84e
Toheeb <toheebyekeen@gmail.com>
2021-12-06 22:24:05 +0100

    added login success comment

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 // const CLIENT_URL = "http://localhost:3000/";
 const CLIENT_URL = "https://intensify-exercise.herokuapp.com/";
 
+//get info on success
 router.get("/login/success", (req, res) => {
   if (req.user) {
     res.status(200).json({
